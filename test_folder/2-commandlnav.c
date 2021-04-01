633e680f3c2734f44b2f47389c23a62ca70678e3
dhreyes <2590@holbertonschool.com>
2021-04-01 21:27:15 +0000

    Fixed task 2 to work properly

---
+++ test_folder/2-commandlnav.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/cdefs.h>
+/**
+ *
+ *
+ *
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+
+	char *token = strtok(argv[1], " "); /*Return first token*/
+
+	while (token != NULL) /*Keep printing tokens*/
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, " ");
+	}
+	return (0);
+}
