a8e92e7ed00e8c05193f8bc96f0ea0308e72198f
yatoro667 <ytorres667@gmail.com>
2021-04-09 16:13:08 +0000

    shell test folder

---
+++ shell_test/shell_init.c
@@ -0,0 +1,18 @@
+#include "shellton.h"
+
+/**
+ * main - initializes shellton
+ *
+ * Return: 0
+ */
+
+int main(int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+	(void)env;
+
+	shelltonprompt();
+
+	return(0);
+}
